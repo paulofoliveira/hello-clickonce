ca86eb446d91138b5e90bf7ae6372d4043cfb892
Paulo Fernando <paulo.fdo@hotmail.com>
2024-01-25 17:50:22 -0300

    UpdateConnectionString

---
+++ build/UpdateConnectionString.ps1
@@ -0,0 +1,32 @@
+﻿param(
+    [Parameter(Mandatory)]
+    [string] $FilePath,
+    [Parameter(Mandatory)]
+    [string] $Name,
+    [Parameter(Mandatory)]
+    [string] $Value
+)
+
+if (!(Test-Path $FilePath) ){
+    throw "File does not exist'"
+}
+
+$cfg = [xml](Get-Content $FilePath)
+
+if ($cfg -eq $null){
+    throw "XML File Configuration does not loaded"
+}
+else
+{
+    $conn = $cfg.configuration.connectionStrings.add|?{$_.name -eq $Name }
+
+    if ($conn -eq $null){
+        throw "ConnectionString does not exist in Configuration'"
+    }
+    else
+    {
+        $conn.connectionString = $Value
+        Write-Host "ConnectionString `"$Name`" updated!" -ForegroundColor Green
+        $cfg.Save($FilePath)
+    }
+}
