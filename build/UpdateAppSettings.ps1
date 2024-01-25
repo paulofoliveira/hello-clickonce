4b6c18a8e8af63db463a310cc7448674fd0c9367
Paulo Fernando <paulo.fdo@hotmail.com>
2024-01-25 17:54:13 -0300

    UpdateAppSettings

---
+++ build/UpdateAppSettings.ps1
@@ -0,0 +1,32 @@
+﻿param(
+    [Parameter(Mandatory)]
+    [string] $FilePath,
+    [Parameter(Mandatory)]
+    [string] $Name,
+    [Parameter(Mandatory)]
+    [string] $Value
+)
+
+if (!(Test-Path $FilePath) ){
+    throw "File does not exist'"
+}
+
+$cfg = [xml](Get-Content $FilePath)
+
+if ($cfg -eq $null){
+    throw "XML File Configuration does not loaded"
+}
+else
+{
+    $key = $cfg.configuration.appSettings.add|?{$_.key -eq $Name }
+
+    if ($key -eq $null){
+        throw "AppSettings does not exist in Configuration'"
+    }
+    else
+    {
+        $key.value = $Value
+        Write-Host "AppSetting `"$Name`" updated!" -ForegroundColor Green
+        $cfg.Save($FilePath)
+    }
+}
