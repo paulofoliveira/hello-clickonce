7765c323443d5fd8fc6195f78676ffe966c4f26d
Paulo Oliveira <paulo.fdo@hotmail.com>
2024-01-26 03:21:35 -0300

    Corrige CustomSecretService.cs

--- HelloClickOnce.App/CustomSecretService.cs
+++ HelloClickOnce.App/CustomSecretService.cs
@@ -2,7 +2,7 @@ namespace HelloClickOnce.App
 {
     internal class CustomSecretService
     {
-        private static readonly string SECRET_KEY = "~[GOS:9OD>2L!T6l>jo*RU%g";
+        private static readonly string SECRET_KEY = "## SECRET HERE ##";
         public string GetSecretKey() => SECRET_KEY;
     }
 }
